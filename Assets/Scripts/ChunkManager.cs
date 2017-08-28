6d603cf84e05e22dc6144461528ae7f0fb39a4a5
Martin Schroeder <martincho48@gmail.com>
2017-08-27 23:20:02 -0300

    + Added todo

---
+++ Assets/Scripts/ChunkManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManager : MonoBehaviour {
+
+	public GameObject [] floors; //ToDo borrar y que sea todo dinamico
+	public Queue<GameObject> chunks;
+	public GameObject [] chunkPrefabs; //ToDo
+
+	public float interval = 2f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("SpawnNextAndDeleteLast");
+		chunks = new Queue<GameObject> (floors);
+
+	}
+
+	public GameObject [] GetChunks() {
+		return chunks.ToArray (); //todo optimize
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator SpawnNextAndDeleteLast() {
+		while (true) {
+			yield return new WaitForSeconds (interval);
+			int prefabIdx = Random.Range (0, chunkPrefabs.Length); //ToDo quen levante todos
+			GameObject lastChunk = chunks.Dequeue ();
+			float lastZ = lastChunk.transform.position.z;
+			Destroy (lastChunk);
+			GameObject newChunk = Instantiate (chunkPrefabs[prefabIdx]);
+			newChunk.transform.position = new Vector3 (newChunk.transform.position.x, newChunk.transform.position.y, lastZ+30f);
+			chunks.Enqueue (newChunk);
+		}
+	}
+
+}
---
+++ Assets/Scripts/FloorMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorMovement : MonoBehaviour {
+
+	public float speed;
+	ChunkManager chunkManager;
+
+	// Use this for initialization
+	void Start () {
+		chunkManager = GetComponent<ChunkManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		GameObject[] floors = chunkManager.GetChunks ();
+		foreach (GameObject f in floors) 
+		{
+			f.transform.position = new Vector3 (f.transform.position.x, f.transform.position.y, f.transform.position.z - speed * Time.deltaTime);
+		}
+	}
+		
+}
