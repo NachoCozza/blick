38ac8dc48dd5fd9cee50227fd90fcd2301cca495
Ignacio Cozza <ignacio.cozza@grayshirts.com.ar>
2017-08-30 23:06:01 -0300

    Fix perspectiva

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -5,30 +5,28 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
 
-    public GameObject[] floors; //ToDo borrar y que sea todo dinamico
     public GameObject[] chunkPrefabs; //ToDo
     public int totalChunks;
 
     GameObject[] chunks;
     float interval = 0f;
     float chunkSize = 10f;
+	PerspectiveController perspective;
 
     // Use this for initialization
     void Awake()
     {
+		perspective = GetComponent<PerspectiveController> ();
         chunks = new GameObject[totalChunks];
         interval = (chunkSize / GetComponent<FloorMovement>().speed) * (totalChunks / 2);
-        float initZ = GetComponent<PerspectiveController>().player.transform.position.z;
+		float initZ = perspective.player.transform.position.z;
         for (int i = 0; i < totalChunks; i++)
         {
-            int prefabIdx = Random.Range(0, chunkPrefabs.Length); //ToDo quen levante todos
-            GameObject newChunk = Instantiate(chunkPrefabs[prefabIdx]);
-            Debug.Log("Z" + initZ + (i * chunkSize));
-            newChunk.transform.position = new Vector3(newChunk.transform.position.x, newChunk.transform.position.y, initZ + (i * chunkSize));
-            Debug.Log(newChunk.transform.position);
-            chunks[i] = newChunk;
+			int prefabIdx = Random.Range(0, chunkPrefabs.Length);
+			GameObject newChunk = Instantiate(chunkPrefabs[prefabIdx]);
+			newChunk.transform.position = new Vector3(newChunk.transform.position.x, newChunk.transform.position.y, initZ + (i * chunkSize));//negrada
+			chunks[i] = newChunk;
         }
-        Debug.Log(interval);
     }
 
     private void Start()
@@ -51,6 +49,24 @@ public class ChunkManager : MonoBehaviour
     {
 
     }
+		
+
+	GameObject InstantiateNewChunk(float zFromDeletedChunk) {
+		return InstantiateNewChunk (zFromDeletedChunk, perspective.Is3D ());
+	}
+
+	GameObject InstantiateNewChunk(float zFromDeletedChunk, bool is3D) {
+		int prefabIdx = Random.Range(0, chunkPrefabs.Length);
+		GameObject res = Instantiate(chunkPrefabs[prefabIdx]);
+		res.transform.position = new Vector3(res.transform.position.x, res.transform.position.y, zFromDeletedChunk + totalChunks * chunkSize);
+		if (is3D) {
+			res.AddComponent<BoxCollider> ();
+		} 
+		else {
+			res.AddComponent<BoxCollider2D> ();
+		}
+		return res;
+	}
 
     IEnumerator SpawnNextAndDeleteLast()
     {
@@ -65,9 +81,7 @@ public class ChunkManager : MonoBehaviour
                 lastZ = toDelete.transform.position.z;
                 Destroy(toDelete);
                 chunks[i] = chunks[newIndex + i];
-                int prefabIdx = Random.Range(0, chunkPrefabs.Length); //ToDo quen levante todos
-                GameObject newChunk = Instantiate(chunkPrefabs[prefabIdx]);
-                newChunk.transform.position = new Vector3(newChunk.transform.position.x, newChunk.transform.position.y, lastZ + totalChunks * chunkSize);
+				GameObject newChunk = InstantiateNewChunk(lastZ);
                 chunks[newIndex + i] = newChunk;
             }
             yield return new WaitForSeconds(interval);
