0f4cee254988a6c7e97f0708126c6333d9b859f3
NachoCozza <ignaciocozza@gmail.com>
2017-09-30 14:04:00 -0300

    Main menu

---
+++ Assets/Scripts/ButtonHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    Text text;
+    Color notHoverColor;
+    void Start() {
+        text = transform.GetChild(0).gameObject.GetComponent<Text>();
+        notHoverColor = text.color;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.color = Color.white;
+    }
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+        text.color = notHoverColor;
+	}
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    public GameObject mainMenu;
+    public GameObject highscores;
+
+	// Use this for initialization
+	void Start () {
+        string scoresText = PlayerPrefs.GetString("scores");
+        if (scoresText != null && scoresText != "") {
+            scoresText = scoresText.Replace("|", "\n");
+        }
+        else {
+            scoresText = "No highscores. GO PLAY!";
+        }
+        Text txt = highscores.transform.GetChild(1).gameObject.GetComponent<Text>();
+        txt.text = scoresText;
+	}
+	
+    public void StartGame() {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void HighScores() {
+		highscores.SetActive(true);
+        mainMenu.SetActive(false);
+        //TODo set scores
+    }
+
+    public void Quit() {
+		Application.Quit();
+    }
+
+    public void MainMenu() {
+        highscores.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+}
